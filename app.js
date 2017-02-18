55c444c1f4013c4ab20a4d2c559f1a7a0ad9710d
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-18 12:05:47 -0700

    Create to do controller

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require('express');
-
-var app = express();
+var express        = require('express');
+var todoController = require('./controllers/todoController')
+var app            = express();
 
 //set up template engine
 app.set('view engine', 'ejs');
@@ -8,6 +8,9 @@ app.set('view engine', 'ejs');
 //static files
 app.use(express.static('./public'));
 
+//fire controllers
+todoController(app);
+
 //listen to port
 app.listen(3000);
-console.log('You are listening to port 3000...');
+console.log('Listening to port 3000...');
