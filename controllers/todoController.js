b9b4df2b21f37b37651a204fe746ee6dd4607b46
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-18 13:03:30 -0700

    Add mongo database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,21 @@
 var bodyParser       = require('body-parser');
+var mongoose         = require('mongoose');
+
+//Connect to the database
+mongoose.connect('mongodb://test:test@ds157349.mlab.com:57349/todo')
+
+//Create a schema
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+//Create Model
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'Get flowers'}).save(function(error){
+  if (error) throw error;
+  console.log('item saved');
+});
+
 var data             = [ {item: 'learn mongo'}, {item: 'organize project'}, {item: 'learn javascript'} ];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
