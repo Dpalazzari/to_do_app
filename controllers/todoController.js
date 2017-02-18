562dced8a5192cf78d6e999d79c1a99c4dbf13cb
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-18 12:43:13 -0700

    Adds post and delete capabilities

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,21 @@
+var bodyParser       = require('body-parser');
+var data             = [ {item: 'learn mongo'}, {item: 'organize project'}, {item: 'learn javascript'} ];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
   app.get('/todo', function(request, response){
-    response.render('todo');
+    response.render('todo', {todos: data});  
   });
 
-  app.post('/todo', function(request, response){
-
+  app.post('/todo', urlencodedParser, function(request, response){
+    data.push(request.body);
+    response.json(data);
   });
 
-  app.delete('/todo', function(request, response){
-
+  app.delete('/todo/:item', function(request, response){
+    data = data.filter(function(todo){ //filter's item out of the array when todo.item === :item in the route
+      return todo.item.replace(/ /g, '-') !== request.params.item; //creates a slug for each item basically
+    });
+    response.json(data);
   });
 };
